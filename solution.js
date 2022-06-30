032bb362264a8916cb1a81d12c548ae55da40010
S Rabi <rabi.sanaye@yahoo.com>
2022-06-30 15:48:33 +0200

    operators-arithmetic-pt1 exercise

--- solution.js
+++ solution.js
@@ -1,1 +1,50 @@
 // write your code in this file
+// Q1.
+const sellTickets = 6450;
+const ticketPrice = 15;
+console.log("The theater sold", sellTickets / ticketPrice, "tickets");
+// Q2.
+const SylviaWeeksIncome = 500;
+const yearIncome = SylviaWeeksIncome * 4 * 12;
+console.log(yearIncome);
+// Q3.
+const calPercentage = (17 / 30) % 100;
+console.log(calPercentage);
+// Q4.
+const oneSideLength = 4.75;
+const squarePerimeter = 4.75 * 4;
+console.log(squarePerimeter);
+// Q5.
+const perimeterTriangle = 5 + 6 + 7;
+console.log(perimeterTriangle);
+// Q6.
+const areaOfSquare = 5 * 4;
+console.log(areaOfSquare);
+// Q7.
+const rightAngledTriangle = 3 + 4 + 5;
+console.log(rightAngledTriangle);
+// Q8.
+const cubeSideLength = 9;
+const cubeVolume = cubeSideLength * 4 * 6;
+console.log(cubeVolume);
+// Q9.
+const firstBill = 22.35 + (22.35 % 10);
+const secBill = 26.67 + (26.67 % 15);
+const thirdBill = 35.92 + (35.92 % 20);
+console.log(firstBill, secBill, thirdBill);
+// Q10.
+const noemyWorkedHours = 8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4;
+const averageWorkedHours = noemyWorkedHours / 10;
+console.log(averageWorkedHours);
+// Q11.
+const studentScore = 75 + 70 + 85 + 90 + 100;
+const fiveTestAverage = studentScore;
+const sixtAverage = 85 * 6;
+const allAverage = sixtAverage - fiveTestAverage;
+
+console.log(allAverage);
+// Q12.
+const firstClassAverage = 80 * 9;
+const currentAverage = 78 * 8;
+const balanceAverage = firstClassAverage - currentAverage;
+console.log("James need to score", balanceAverage, "to get 80%");
